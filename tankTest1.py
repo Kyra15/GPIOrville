2fa8c3cab12e0e3d2e3ab33dd1719921e0f7bfd9
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2023-10-03 08:06:35 -0400

    Create tankTest1.py

---
+++ tankTest1.py
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+from time import sleep
+
+import OPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.SUNXI)
+
+motor1fwd = 'PA7'
+motor1rev = 'PA8'
+motor2fwd = 'PA9'
+motor2rev = 'PA10'
+
+fwdmotor = [motor1fwd, motor2fwd]
+revmotor = [motor1rev, motor2rev]
+
+GPIO.setup(motor1fwd, GPIO.OUT)
+GPIO.setup(motor2fwd, GPIO.OUT)
+
+print("Finished Setup")
+GPIO.output(fwdmotor, GPIO.HIGH)
+
+sleep(2)
+
+print("Finished FwdMotors")
+
+GPIO.output(fwdmotor, GPIO.LOW)
+GPIO.output(revmotor, GPIO.LOW)
+
+print("Done")
