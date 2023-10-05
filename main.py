a515349c392e62542f6f611369750c436b5eca65
Kyra15 <pl251201@ahschool.com>
2023-10-05 08:41:12 -0400

    Commit1 rasp

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
+pwm = GPIO.PWM(24, 200)
+pwm.start(50)
+
 motor1fwd = 21
 motor1rev = 22
 motor2fwd = 23
-motor2rev = 24
-
-#pwm pin
+motor2rev = 24 #pwm pin
 
 fwdmotor = [motor1fwd, motor2fwd]
 revmotor = [motor1rev, motor2rev]
 
 GPIO.setup(motor1fwd, GPIO.OUT)
 GPIO.setup(motor2fwd, GPIO.OUT)
+GPIO.setup(motor1rev, GPIO.OUT)
+GPIO.setup(motor2rev, GPIO.OUT)
 
 print("Finished Setup")
 GPIO.output(fwdmotor, GPIO.HIGH)
@@ -26,5 +29,6 @@ print("Finished FwdMotors")
 
 GPIO.output(fwdmotor, GPIO.LOW)
 GPIO.output(revmotor, GPIO.LOW)
+pwm.stop()
 
 print("Done")
