a51359125db202ca2da8d939094a8ffd2f5cda49
Kyra15 <pl251201@ahschool.com>
2023-10-23 08:35:27 -0400

    commit 10/23

--- tankClass.py
+++ tankClass.py
@@ -1,16 +1,16 @@
 from time import sleep
 
-import OPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 
 class Tank:
     def __init__(self):
-        GPIO.setmode(GPIO.SUNXI)
+        GPIO.setmode(GPIO.BCM)
 
-        self.motor1fwd = 'PA7'
-        self.motor1rev = 'PA8'
-        self.motor2fwd = 'PA9'
-        self.motor2rev = 'PA10'
+        self.motor1fwd = 21
+        self.motor1rev = 22
+        self.motor2fwd = 23
+        self.motor2rev = 24 #pwm pin
         self.fwdmotor = [self.motor1fwd, self.motor2fwd]
         self.revmotor = [self.motor1rev, self.motor2rev]
 
