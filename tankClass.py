7dc4ed5fbc2d185caebb6266593e1f19e09fe685
Kyra15 <pl251201@ahschool.com>
2023-10-25 22:59:37 -0400

    Commit2 from home

--- tankClass.py
+++ tankClass.py
@@ -1,12 +1,17 @@
+# import time for delays
 from time import sleep
 
+# import the raspberrypi gpio library
 import RPi.GPIO as GPIO
 
 
+# create the tank class
 class Tank:
+    # when the program start, set the gpio mode to bcm so you can use numbers for pins
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
 
+        # set pin numbers for each wire (these are approximations and will be update once we get the robot working)
         self.motor1fwd = 21
         self.motor1rev = 22
         self.motor2fwd = 23
@@ -14,13 +19,16 @@ class Tank:
         self.fwdmotor = [self.motor1fwd, self.motor2fwd]
         self.revmotor = [self.motor1rev, self.motor2rev]
 
+        # set up the motors as outputs
         GPIO.setup(self.motor1fwd, GPIO.OUT)
         GPIO.setup(self.motor2fwd, GPIO.OUT)
 
+    # define the forward function, which outputs high to the forward motors for as much time as is inputted
     def forward(self, time):
         GPIO.output(self.fwdmotor, GPIO.HIGH)
         sleep(time)
 
+    # define the stop function, which just sets the output for all motors to low.
     def stop(self):
         GPIO.output(self.fwdmotor, GPIO.LOW)
         GPIO.output(self.revmotor, GPIO.LOW)
--- tankMain.py
+++ tankMain.py
@@ -1,4 +1,8 @@
+# import tank class
 from tankClass import Tank
 
+# create a tank object
 tank = Tank()
+
+# move the tank forward for 3 seconds
 tank.forward(3)
