911f3cb00fbded05654f1274574519b9fc4c3379
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2023-11-06 16:06:50 -0500

    Update tankClass.py

--- tankClass.py
+++ tankClass.py
@@ -1,28 +1,36 @@
+# import necessary libraries
 from time import sleep
 import board
 from adafruit_motorkit import MotorKit
 
-
+# create tank class
 class Tank:
+    # initialize the motor kit library
     def __init__(self):
         self.kit = MotorKit(0x40)
 
-    
+    # define the forward function, which takes parameters time and 2 different speeds for each wheel. If nothing is provided, the default values are used.
     def forward(self, time, speed1=0.752, speed2=0.652):
         self.kit.motor1.throttle = speed1
-        self.kit.motor2.throttle = speed2
+        self.kit.motor2.throttle = -speed2
         sleep(time)
 
-
+    # define the leftturn function, which takes parameters time and 2 different speeds for each wheel. The function 
     def leftturn(self, time=0.71, speed1=0.6, speed2=0.75):
+        self.kit.motor1.throttle = -speed1
+        self.kit.motor2.throttle = -speed2
+        sleep(time)
+
+    # define the rightturn function, which takes parameters time and 2 different speeds for each wheel
+    def rightturn(self, time=0.71, speed1=0.6, speed2=0.75):
         self.kit.motor1.throttle = speed1
         self.kit.motor2.throttle = speed2
         sleep(time)
 
     
-    def leftturn(self, time=0.71, speed1=-0.6, speed2=-0.75):
-        self.kit.motor1.throttle = speed1
-        self.kit.motor2.throttle = speed2
+    def backward(self, time, peed1=0.752, speed2=-0.652):
+        self.kit.motor1.throttle = -speed1
+        self.kit.motor2.throttle = -speed2
         sleep(time)
 
 
