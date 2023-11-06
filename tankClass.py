9a4e6e7bb0b1686ba18b0ba104c5f925908e36c9
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2023-11-06 15:57:24 -0500

    Update tankClass.py 11/6/23

--- tankClass.py
+++ tankClass.py
@@ -5,7 +5,7 @@ from adafruit_motorkit import MotorKit
 
 class Tank:
     def __init__(self):
-        self.kit = MotorKit(i2c=board.I2C(), pwm_frequency=speed)
+        self.kit = MotorKit(0x40)
 
     
     def forward(self, time, speed1=0.752, speed2=0.652):
@@ -14,19 +14,20 @@ class Tank:
         sleep(time)
 
 
-    def leftturn(self, speed1=0.6, speed2=0.75):
+    def leftturn(self, time=0.71, speed1=0.6, speed2=0.75):
         self.kit.motor1.throttle = speed1
         self.kit.motor2.throttle = speed2
-        sleep(0.71)
+        sleep(time)
 
     
-    def leftturn(self, speed1=-0.6, speed2=-0.75):
+    def leftturn(self, time=0.71, speed1=-0.6, speed2=-0.75):
         self.kit.motor1.throttle = speed1
         self.kit.motor2.throttle = speed2
-        sleep(0.71)
+        sleep(time)
 
 
-    def stop(self):
+    def stop(self, time):
         self.kit.motor1.throttle = 0
         self.kit.motor2.throttle = 0
+        sleep(time)
 
