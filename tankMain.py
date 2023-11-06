fe73d2856c77b2c4f080b7eedbcac62f68266063
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2023-11-06 16:02:10 -0500

    Commented tankMain.py

--- tankMain.py
+++ tankMain.py
@@ -1,7 +1,23 @@
+# import tank class file
 from tankClass import Tank
 
+# create tank object
 tank = Tank()
+
+# move the tank forward for 2.5 seconds & stop
 tank.forward(2.5)
 tank.stop(2)
+
+# turn the tank to the left & stop 
 tank.turnleft()
 tank.stop(2)
+
+# move the tank forward for 2.1 seconds & stop
+tank.forward(2.1)
+tank.stop(2)
+
+# move the tank backwards for 4 seconds & stop
+tank.backward(4)
+tank.stop(2)
+
+
